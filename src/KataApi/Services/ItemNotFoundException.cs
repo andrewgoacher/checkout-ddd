358ecf78173e80516c8bc59e371b00a764fd1eb9
Andrew Goacher <goach@hey.com>
2021-05-10 22:20:20 +0100

    Implement basic item service for the kata

---
+++ src/KataApi/Services/ItemNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Kata.Domain.Checkout;
+
+namespace KataApi.Services
+{
+    public class ItemNotFoundException : Exception
+    {
+        public ItemNotFoundException(ItemId itemId) : base($"Item with id ({itemId}) not found")
+        {
+        }
+    }
+}
--- src/KataApi/Services/ItemService.cs
+++ src/KataApi/Services/ItemService.cs
@@ -1,19 +1,35 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Kata.Domain.Checkout;
 using Kata.Domain.Services;
 
 namespace KataApi.Services
 {
+    using Item = Kata.Domain.Services.Models.Item;
+
     public class ItemService : IItemService
     {
+        private readonly Dictionary<ItemId, Item> _items;
+
         public ItemService()
         {
+            _items = new()
+            {
+                { new("A"), new() { Id = new("A"), Price = new(50) } },
+                { new("B"), new() { Id = new("B"), Price = new(30) } },
+                { new("C"), new() { Id = new("C"), Price = new(20) } },
+                { new("D"), new() { Id = new("D"), Price = new(15) } }
+            };
         }
 
-        public Task<Kata.Domain.Services.Models.Item> FetchItemAsync(ItemId itemId)
+        public Task<Item> FetchItemAsync(ItemId itemId)
         {
-            throw new NotImplementedException();
+            if (_items.TryGetValue(itemId, out var item))
+            {
+                return Task.FromResult(item);
+            }
+
+            throw new ItemNotFoundException(itemId);
         }
     }
 }
