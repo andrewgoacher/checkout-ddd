1b6d2ed4bf4bddf20023d9879af6bb607b857c8d
Andrew Goacher <goach@hey.com>
2021-05-13 22:22:14 +0100

    Add a method to add an item to the basket through the API

--- src/KataApi/Controllers/CheckoutController.cs
+++ src/KataApi/Controllers/CheckoutController.cs
@@ -23,8 +23,10 @@ namespace KataApi.Controllers
         /// </summary>
         ///
         /// <remarks>
+        /// 
         /// Sample request:
         /// POST /api/checkout
+        /// 
         /// </remarks>
         ///
         /// <returns>A basket id</returns>
@@ -42,18 +44,22 @@ namespace KataApi.Controllers
         /// </summary>
         ///
         /// <remarks>
+        /// 
         /// Sample request:
         /// POST /api/checkout/{basketId}
+        /// 
         /// </remarks>
         /// 
         /// <param name="basketId">The id of the basket</param>
         /// <returns>The complete basket</returns>
         /// <response code="200">The basket</response>
+        /// <response code="400">There was a validation error</response>
         /// <response code="404">The basket could not be found</response>
         [HttpGet]
         [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
         [ProducesResponseType(404)]
-        public async Task<ActionResult<Basket>> GetBasket(Guid basketId)
+        public async Task<ActionResult<Basket>> GetBasket([FromRoute] Guid basketId)
         {
             var basket = await _appService.GetBasket(new BasketId(basketId));
             if (basket == null)
@@ -63,5 +69,45 @@ namespace KataApi.Controllers
 
             return new OkObjectResult(basket);
         }
+
+        /// <summary>
+        /// Adds an item with a specified quantity to the specified basket
+        /// </summary>
+        ///
+        /// <remarks>
+        ///
+        /// Sample request:
+        /// /api/checkout/{basketId}
+        /// {
+        ///     "ItemId": "A",
+        ///     "Quantity": 5
+        /// }
+        /// 
+        /// </remarks>
+        /// 
+        /// <param name="basketId">The basket to add the item to</param>
+        /// <param name="addItem">
+        /// Contains the item id of the item to add and a quantity of the item to add
+        /// </param>
+        ///
+        /// <response code="200">Returns OK if the item was added</response>
+        /// <response code="400">A validation error occured when trying to add the item to the basket</response>
+        /// <response code="404">Returns not found if the basket could not be found</response>
+        /// 
+        /// <returns></returns>
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> AddItem([FromRoute] Guid basketId, [FromBody] AddItemCmd addItem)
+        {
+            await _appService.AddItemAsync(new (basketId), new (addItem.ItemId), new (addItem.Quantity));
+            return new OkResult();
+        }
+    }
+
+    public class AddItemCmd
+    {
+        public string ItemId { get; set; }
+        public int Quantity { get; set; }
     }
 }
