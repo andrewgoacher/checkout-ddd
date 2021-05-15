1a39a4f118440fece0eeb8bbace72a5b4aba0277
Andrew Goacher <goach@hey.com>
2021-05-15 14:39:09 +0100

    Implement best practice with class fixture for integration test

--- tests/Kata.IntegrationTests/CheckoutControllerTests.cs
+++ tests/Kata.IntegrationTests/CheckoutControllerTests.cs
@@ -1,37 +1,19 @@
 ﻿using System;
-using System.IO;
 using System.Net.Http;
-using System.Reflection;
 using System.Threading.Tasks;
-using Kata.Domain.Checkout;
-using KataApi;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.Configuration;
+using Kata.IntegrationTests.Fixtures;
 using Xunit;
 
 namespace Kata.IntegrationTests
 {
     public class CheckoutControllerTests
+        : IClassFixture<TestServerFixture>
     {
-        private readonly TestServer _server;
         private readonly HttpClient _client;
 
-        public CheckoutControllerTests()
+        public CheckoutControllerTests(TestServerFixture fixture)
         {
-            string codeBase = Assembly.GetExecutingAssembly().Location;
-            var dir =  Path.GetDirectoryName(codeBase);
-
-            _server = new TestServer(new WebHostBuilder()
-                .UseEnvironment("Development")
-                .UseConfiguration(new ConfigurationBuilder()
-                    .SetBasePath(dir)
-                    .AddJsonFile("appsettings.json")
-                    .Build()
-                ) 
-                .UseStartup<Startup>());
-
-            _client = _server.CreateClient();
+            _client = fixture.Client;
         }
 
         [Fact]
@@ -45,10 +27,6 @@ namespace Kata.IntegrationTests
             var id = System.Text.Json.JsonSerializer.Deserialize<Guid>(responseString);
 
             Assert.NotEqual(Guid.Empty, id);
-
-
-
-            var basketId = new BasketId(id);
         }
     }
 }
---
+++ tests/Kata.IntegrationTests/Fixtures/TestServerFixture.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Reflection;
+using KataApi;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+
+namespace Kata.IntegrationTests.Fixtures
+{
+    public class TestServerFixture : IDisposable
+    {
+        private readonly TestServer _server;
+        private readonly HttpClient _client;
+
+        private bool disposedValue;
+
+        public TestServerFixture()
+        {
+            _server = new TestServer(new WebHostBuilder()
+             .UseEnvironment("Development")
+             .UseConfiguration(new ConfigurationBuilder()
+                 .SetBasePath(GetTestBaseDir())
+                 .AddJsonFile("appsettings.json")
+                 .Build()
+             )
+             .UseStartup<Startup>());
+
+            _client = _server.CreateClient();
+        }
+
+        public TestServer TestServer => _server;
+        public HttpClient Client => _client;
+
+        private static string GetTestBaseDir() =>
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _client?.Dispose();
+                    _server?.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
