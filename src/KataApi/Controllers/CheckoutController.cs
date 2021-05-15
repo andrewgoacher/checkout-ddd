61bd0ff732220b7a128b361d7086a97832db1509
Andrew Goacher <goach@hey.com>
2021-05-15 12:53:15 +0100

    Update schema from controllers

--- src/KataApi/Controllers/CheckoutController.cs
+++ src/KataApi/Controllers/CheckoutController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Kata.Domain.Checkout;
 using Kata.Domain.Services;
+using KataApi.ProblemDetails;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KataApi.Controllers
@@ -32,7 +33,9 @@ namespace KataApi.Controllers
         /// <returns>A basket id</returns>
         /// <response code="200">The id of the newly created basket</response>
         [HttpPost]
-        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(Guid), 200)]
+        [ProducesResponseType(typeof(DomainProblemDetails), 400)]
+        [ProducesResponseType(typeof(InternalServerErrorProblemDetails), 500)]
         public async Task<ActionResult<Guid>> CreateBasket()
         {
             var id = await _appService.CreateBasket();
@@ -49,9 +52,11 @@ namespace KataApi.Controllers
         /// <response code="400">There was a validation error</response>
         /// <response code="404">The basket could not be found</response>
         [HttpGet("{basketId}")]
-        [ProducesResponseType(200)]
-        [ProducesResponseType(400)]
-        [ProducesResponseType(404)]
+        [ProducesResponseType(typeof(Basket), 200)]
+        [ProducesResponseType(typeof(DomainValidationProblemDetails), 400)]
+        [ProducesResponseType(typeof(DomainProblemDetails), 400)]
+        [ProducesResponseType(typeof(DomainNotFoundProblemDetails), 404)]
+        [ProducesResponseType(typeof(InternalServerErrorProblemDetails), 500)]
         public async Task<ActionResult<Basket>> GetBasket([FromRoute] Guid basketId)
         {
             return await _appService.GetBasket(new BasketId(basketId));
@@ -73,7 +78,10 @@ namespace KataApi.Controllers
         /// <returns></returns>
         [HttpPost("{basketId}")]
         [ProducesResponseType(200)]
-        [ProducesResponseType(404)]
+        [ProducesResponseType(typeof(DomainValidationProblemDetails), 400)]
+        [ProducesResponseType(typeof(DomainProblemDetails), 400)]
+        [ProducesResponseType(typeof(DomainNotFoundProblemDetails), 404)]
+        [ProducesResponseType(typeof(InternalServerErrorProblemDetails), 500)]
         public async Task<IActionResult> AddItem([FromRoute] Guid basketId, [FromBody] AddItemCmd addItem)
         {
             await _appService.AddItemAsync(new (basketId), new (addItem.ItemId), new (addItem.Quantity));
