d5ebac1ff522ab017487a611c7f0b2521f14f65f
Andrew Goacher <andrew.goacher@uinsure.co.uk>
2021-05-27 09:48:33 +0100

    Made start with a functional version of the lib

---
+++ Kata.Domain.Fs/Checkout.fs
@@ -0,0 +1,109 @@
+﻿module Kata.Domain.Checkout
+
+open Core
+
+    type DiscountId = private DiscountId of System.Guid
+    type BasketId = private BasketId of System.Guid
+    type ItemId = private ItemId of string
+
+    type Item = private {
+        ParentId: BasketId;
+        Id: ItemId;
+        Price: Price;
+        Quantity: Quantity;
+    }
+
+    type Discount = private {
+        ParentId: BasketId;
+        Id: DiscountId;
+        Amount: Money;
+        Description: Description;
+    }
+
+    type Basket = private {
+        Id: BasketId;
+        Items: Item list;
+        Discounts: Discount list;
+    }
+
+    type DiscountEvents =
+        | DiscountCreated of DiscountId * BasketId * Description * Money
+
+    type ItemEvents =
+        | ItemCreated of BasketId * ItemId * Price * Quantity
+        | QuantityIncremented of Quantity
+        | QuantityDecremeneted of Quantity
+
+    type BasketEvents =
+        | BasketCreated of BasketId
+        | ItemAdded of ItemId  * Price * Quantity
+        | ItemQuantityIncreased of ItemId * Quantity
+        | ItemQuantityReduced of ItemId * Quantity
+        | DiscountsRemoved
+        | DiscountAdded of DiscountId * Money * Description
+
+    type CheckoutEvent =
+        | DiscountEvent of DiscountEvents
+        | ItemEvent of ItemEvents
+        | BasketEvent of BasketEvents
+
+    module ItemId =
+        let createItemId id =
+            if System.String.IsNullOrEmpty id then
+                Error <| Validation "Item Id cannot be an empty string"
+            elif id.Length > 1 then
+                Error <| Validation "Item Id should be a single character"
+            else
+                Ok <| ItemId id
+
+    module Item =
+        let newItem (itemEvent: ItemEvents) =
+            match itemEvent with
+            | ItemCreated (basketId, itemId, price, quantity) ->
+                Ok { ParentId = basketId; Id = itemId; Price = price; Quantity = quantity }
+            | _ -> Error <| GenericError "Cannot process any other events"
+
+        let apply (item:Item) (evt: CheckoutEvent) =
+            match evt with
+            | ItemEvent e ->
+                match e with
+                | QuantityIncremented q ->
+                    let newQuantity = Quantity.incrementBy item.Quantity q
+                    match newQuantity with
+                    | Error e -> Error e
+                    | Ok quantity -> Ok { item with Quantity = quantity}
+                | QuantityDecremeneted q ->
+                    let newQuantity = Quantity.decrementBy item.Quantity q
+                    match newQuantity with
+                    | Error e -> Error e
+                    | Ok quantity -> Ok { item with Quantity = quantity}
+            | _ -> Error <| GenericError "Unable to process this event"
+
+        let total item =
+            Quantity.cost item.Quantity item.Price
+
+    module DiscountId =
+        let createDiscountId id =
+            if id = System.Guid.Empty then
+                Error <| Validation "Invalid id"
+            else
+                Ok <| DiscountId id
+
+    module Discount =
+        let newDiscount (discountEvent: DiscountEvents) =
+            match discountEvent with
+            | DiscountCreated (discountId, basketId, description, price) ->
+                Ok { ParentId = basketId; Id = discountId; Amount = price; Description = description }
+
+    module BasketId =
+        let createBasketId id =
+            if id = System.Guid.Empty then
+                Error <| Validation "Invalid id"
+            else
+                Ok <| BasketId id
+
+    module Basket =
+        let newBasket (basketEvent: BasketEvents) =
+            match basketEvent with
+            | BasketCreated id -> Ok { Id = id; Items = []; Discounts = []}
+            | _ -> Error <| GenericError "Cannot process this event"
---
+++ Kata.Domain.Fs/Core.fs
@@ -0,0 +1,66 @@
+﻿module Kata.Domain.Core
+type DomainError =
+    | AggregateNotFound of string
+    | EntityNotFound of string
+    | Validation of string
+    | GenericError of string
+
+type Quantity = private Quantity of int
+type Money = private Money of decimal
+type Price = private Price of Money
+type Description = private Description of string
+
+module Money =
+
+    let createMoney (amount:decimal) =
+        if System.Math.Round(amount,2) <> amount then
+            Error <| Validation "Too many decimal places"
+        else
+            Ok <| Money amount
+
+module Price =
+
+    let createPrice amount =
+        if amount <= 0m then
+            Error <| Validation "Price must be more than 0"
+        else
+            match Money.createMoney amount with
+            | Error e -> Error e
+            | Ok money -> Ok <| Price money
+
+module Description =
+    let createDescription desc =
+        if System.String.IsNullOrEmpty desc then
+            Error <| Validation "Description should not be an empty string"
+        else
+            Ok <| Description desc
+
+module Quantity =
+
+    let createQuantity amount =
+        if amount < 0 then
+            Error <| Validation "Amount should be greater than 0"
+        else
+            Ok <| Quantity amount
+
+    let increment qty amount =
+        let (Quantity q) = qty
+        createQuantity (q + amount)
+
+    let incrementBy qty1 qty2 =
+        let (Quantity q1) = qty1
+        let (Quantity q2) = qty2
+        createQuantity (q1 + q2)
+
+    let decrementBy qty1 qty2 =
+        let (Quantity q1) = qty1
+        let (Quantity q2) = qty2
+        createQuantity (q1 - q2)
+
+    let cost qty price =
+        let (Quantity q) = qty
+        let (Price money) = price
+        let (Money amount) = money
+        match Money.createMoney ((decimal)q * amount) with
+        | Ok money -> money
+        | _ -> Money 0m
